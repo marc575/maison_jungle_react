aa9aca2d8cc7056e41645b89142c041b3b2e2a32
marc575 <mtatchou7@gmail.com>
2024-12-20 05:37:12 +0100

    fix responsive

--- src/components/Plant.js
+++ src/components/Plant.js
@@ -9,7 +9,7 @@ function Plant ({name, cover, id, light, water, price, description}) {
             <span className='lmj-plant-item-price'>{price} €</span>
             <img className='lmj-plant-item-cover' src={cover} alt={`${name} cover`} />
             <h2 onClick={() => handleClick(name) }>{name}</h2> 
-            <ReadMore text={description} maxLength={100} />
+            <ReadMore text={description} maxLength={80} />
             <div className='lmj-plant-item-elt'>
                 <CareScale careType='water' scaleValue={water} />
 		        <CareScale careType='light' scaleValue={light} />
